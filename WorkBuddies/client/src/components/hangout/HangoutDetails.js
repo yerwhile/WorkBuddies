d0d39c0a69b7005bb80732452cf2c65c576b272c
Matthew McDonald <yerwhile@gmail.com>
2023-05-23 15:34:22 -0500

    continue work on hangout details js

--- WorkBuddies/client/src/components/hangout/HangoutDetails.js
+++ WorkBuddies/client/src/components/hangout/HangoutDetails.js
@@ -15,7 +15,6 @@ export default function HangoutDetails() {
     
     const [hangout, setHangout] = useState({});
     const [currentUser, setCurrentUser] = useState({});
-    const [buddyCount, setBuddyCount] = useState(0);
     
     useEffect(() => {
         getHangoutDetails(id).then(setHangout);
@@ -65,38 +64,19 @@ export default function HangoutDetails() {
                                 </tbody>
                             </Table>
                         </div>
-                        <div className="hangout_profile__buttons">
+                        <div className="hangout_details__buttons">
                             <div className="pack_profile__edit">
-                                {
-                                    isMember
-                                        ? <Button color="secondary" href={`../editPack/${id}`}>Edit Pack</Button>
-                                        : ""
-                                    
-                                }
-                            </div>
-                            <div className="pack_profile__joinleave">
-                                {
-                                    isMember === true
-                                        ? handleLeaveButton()
-                                        : ""
-                                    
-                                }
-                                {
-                                    isMember === false && pack.isOpen
-                                        ? handleJoinButton()
-                                        : ""
-                                    
-                                }
+                                <Button color="secondary" href={`../editHangout/${id}`}>Edit Hangout</Button>
                             </div>
                         </div>
                     </div>
-                    <div className="pack_vibes">
-                        <h4>Pack Vibes</h4>
-                        <div className="pack_vibes__list">
+                    <div className="hangout_details__vibes">
+                        <h4>Hangout Vibes</h4>
+                        <div className="hangout_details__vibesList">
                             <Table variant="flush">
                                 <tbody>
                                     <tr>
-                                        {pack?.vibes?.map((vibe) => {
+                                        {hangout?.vibes?.map((vibe) => {
                                             return <tr key={vibe.id}>{vibe.name}</tr>
                                         })}
                                     </tr>
@@ -105,7 +85,7 @@ export default function HangoutDetails() {
                             
 
                         </div>
-                        <div className="pack_vibes__button">
+                        <div className="hangout_details__vibesButton">
                             {
                                 isMember == true
                                     ? <>
@@ -117,52 +97,24 @@ export default function HangoutDetails() {
                     </div>
             </div>
                     
-            <div className="pack_profile__right">
-                <div className="pack_buddies">
-                <h4>Buddies in {pack.name}</h4>
+            <div className="hangout_details__right">
+                <div className="hangout_packs">
+                <h4>Packs associated with {hangout.name}</h4>
                     <Table striped bordered>
                         <thead>
                             <tr>
                                 <th>Name</th>
-                                <th>Location</th>
-                                <th>Company</th>
+                                <th>Formed</th>
+                                <th>Buddy Count</th>
                                 <th>Details</th>
                             </tr>
                         </thead>
                         <tbody>
-                            {pack?.buddies?.map((buddy) => <PackBuddies key={buddy.id} buddy={buddy} buddyCount={buddyCount} />
-                                
-                                )}
+                            {/* {buddy?.packs?.map((pack) => <ProfilePacks key={pack.id} pack={pack} />
+                            
+                            )} */}
                         </tbody>
-                        
                     </Table>
-                </div>
-                <div className="pack_hangouts">
-                    <div className="pack_hangouts__list">
-                    <h4>Hangouts of {pack.name}</h4>
-                        <Table striped bordered>
-                            <thead>
-                                <tr>
-                                    <th>Name</th>
-                                    <th>Street Address</th>
-                                    <th>City</th>
-                                    <th>State</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {pack?.hangouts?.map((hangout) => <PackHangouts key={hangout.id} hangout={hangout}/>)}
-                            </tbody>
-                        </Table>
-                    </div>
-                    <div className="pack_hangouts__button">
-                        {
-                            isMember == true
-                                ? <>
-                                    <Button color="secondary" href={`../editHangouts/${id}`}>Edit Hangouts</Button>
-                                    </>
-                                : ""
-                        }
-                    </div>
                 </div>
             </div>
             
